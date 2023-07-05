c7937bbead1ad3318e672e75c01132a2632aedbf
ruth-veiga <137335194+ruth-veiga@users.noreply.github.com>
2023-07-05 17:22:20 -0300

    Initial commit

---
+++ projeto_compara/core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Cidade)
+admin.site.register(Categoria)
+admin.site.register(Mercado)
+admin.site.register(Produto)
+admin.site.register(produtos_mercados)
